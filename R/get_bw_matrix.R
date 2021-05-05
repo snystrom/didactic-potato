7d157bf49c52614d7cd41708de8a9aaed9a81237
snystrom <snystrom@users.noreply.github.com>
2021-05-05 12:04:07 -0400

    initial commit

---
+++ R/get_bw_matrix.R
@@ -0,0 +1,61 @@
+#' Get signal matrix for regions using bigwig input
+#'
+#' @param bw an [rtracklayer::BigWigFile], [rtracklayer::BigWigFileList] or path to bigwig file
+#' @param regions a GRanges object (all regions must be equal width)
+#' @param type one of: c("mean", "min", "max", "coverage", "sd")
+#'
+#' @return a matrix of bigwig signal
+#' @export
+#'
+#' @examples
+get_bw_matrix <- function(bw, regions,
+                          type = c("mean", "min", "max", "coverage", "sd")){
+  UseMethod("get_bw_matrix")
+}
+
+#' @noRd
+#' @export
+get_bw_matrix.character <- function(bw, regions,
+                          type = c("mean", "min", "max", "coverage", "sd")){
+  get_bw_matrix.BigWigFile(rtracklayer::BigWigFile(bw),
+                           regions, type)
+
+}
+
+#' @noRd
+#' @export
+get_bw_matrix.BigWigFileList <- function(bw, regions,
+                          type = c("mean", "min", "max", "coverage", "sd")){
+  lapply(bw, get_bw_matrix, regions, type)
+  # TODO: consider 3d matrix output:
+  # simplify2array()
+}
+
+#' @noRd
+#' @export
+#' @import GenomicRanges
+#' @importFrom rtracklayer summary
+get_bw_matrix.BigWigFile <- function(bw, regions,
+                          type = c("mean", "min", "max", "coverage", "sd")){
+  type <- match.arg(type, choices = c("mean", "min", "max", "coverage", "sd"))
+
+  # TODO: expect input is a BigWigFile,
+  # need to coerce path to this before passing
+  #bw <- rtracklayer::BigWigFile(path)
+
+  size <- unique(width(regions))
+  if (length(size) != 1) {
+    stop("All regions must be equal width", call. = FALSE)
+  }
+
+
+  # not 100% sure how the seqlevels thing works, need to do some reality
+  # checking that no errors introduced here: WRITE TESTS!! (but for what?)
+  seqlevels(regions) <- seqlevels(bw)
+  seqinfo(regions) <- seqinfo(bw)
+
+  # error if widths are not all unique in in_regions
+  rtracklayer::summary(bw, which = regions, as = "matrix",
+                       type = type, size = size)
+
+}
---
+++ R/utils.R
@@ -0,0 +1,11 @@
+get_file_type <- function(tracks){
+  # can be:
+  # rtracklayer::BigWigFile
+  # Rsamtools::BamFile
+  # character vector:
+  #  - .bam$
+  #  - .bw$
+}
+
+
+
