84acfb2deca607c838346fba425be0d93e9668df
snystrom <snystrom@users.noreply.github.com>
2024-11-25 22:08:02 -0500

    Style, Lint, Doc.

--- tests/testthat/test-get_bw_matrix.R
+++ tests/testthat/test-get_bw_matrix.R
@@ -2,14 +2,14 @@ test_that("bw matrix is correct", {
   # TODO: can we use rtracklayer test bigwigs??
   bw <- system.file("tests", "test.bw", package = "rtracklayer")
   bw <- rtracklayer::BigWigFile(bw)
-  regions <- GRanges(c("chr2", "chr2"), IRanges::IRanges(c(1,101), c(100, 200)))
+  regions <- GRanges(c("chr2", "chr2"), IRanges::IRanges(c(1, 101), c(100, 200)))
 
   # type errors correctly
   expect_error(get_bw_matrix(bw, regions, type = "mon"))
-  #rtracklayer::import(bw, which = regions)
+  # rtracklayer::import(bw, which = regions)
 
   # Ensure catching missing chromosomes
-  bad_regions <- GRanges(c("chr3", "chr3"), IRanges::IRanges(c(1,101), c(100, 200)))
+  bad_regions <- GRanges(c("chr3", "chr3"), IRanges::IRanges(c(1, 101), c(100, 200)))
   expect_error(get_bw_matrix(bw, bad_regions), "do not match chromosomes")
 
   # Catch out of bounds
